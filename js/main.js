4902af9ca738b0a73223a30afc8d0a0df8818a08
Sam <splavigne@gmail.com>
2014-12-04 15:47:47 -0500

    use classes for visualizers

--- js/main.js
+++ js/main.js
@@ -2,57 +2,126 @@ var index = 0;
 var interval = 1000;
 var data;
 
-var width = 1500;
-var height = 100;
-var margin =  {top: 80, right: 80, bottom: 80, left: 80};
+var visualizers = [];
 
-var	x = d3.scale.linear().range([0, width*50]);
-var	y = d3.scale.linear().range([height, 0]);
-
-var circle, svg, line;
+/*
+ *
+ * Loads a csv from a url, sets up visualizers
+ *
+ */
 
 function load_csv(url) {
-  d3.csv(url, start);
+  d3.csv(url, function(error, dataset){
+    data = dataset;
+    visualizers.push(new GlobalGoldstein());
+    visualizers.push(new MainText());
+
+    advance();
+  });
 }
 
-function start(error, dataset) {
-  data = dataset;
 
-  x.domain([1, data.length+1]);
-	y.domain([-10.0, 10.0]);
+/*
+ *
+ * Advances to the next data item
+ *
+ */
 
-  line = d3.svg.line()
+function advance() {
+  visualizers.forEach(function(v) {
+    v.update();
+  });
+
+  index ++;
+  if (index >= data.length) index = 0;
+
+  setTimeout(advance, interval);
+}
+
+
+/*
+ *
+ * GlobalGoldstein
+ * Graph the whole novel with goldstein score over time
+ *
+ */
+
+function GlobalGoldstein() {
+  this.width = 1500;
+  this.height = 100;
+  this.margin = {top: 80, right: 80, bottom: 80, left: 80};
+
+  var x = this.x = d3.scale.linear().range([0, this.width*50]);
+  var y = this.y = d3.scale.linear().range([this.height, 0]);
+
+  this.x.domain([1, data.length+1]);
+	this.y.domain([-10.0, 10.0]);
+
+  this.line = d3.svg.line()
     .x(function(d) { return x(+d.num); })
     .y(function(d) { return y(+d.goldstein_score); });
 
-  svg = d3.select('body')
+  this.svg = d3.select('body')
     .append('svg')
-      .attr('width', width)
-      .attr('height', height)
+      .attr('width', this.width)
+      .attr('height', this.height)
 
-  svg.append('g')
+  this.svg.append('g')
     .attr('class', 'holder')
     .attr('transform', 'translate(0,0)')
 
-  svg.select('g').append('path')
+  this.svg.select('g').append('path')
     .datum(data.slice(0, 100))
 		.attr('class', 'line')
-		.attr('d', line);
+		.attr('d', this.line);
 
-  circle = svg.select('g').append('circle')
+  this.circle = this.svg.select('g').append('circle')
     .data([data[index]])
     .attr('class', 'position-marker')
     .attr('cx', function(d){ return x(+d.num)})
     .attr('cy', function(d){ return y(+d.goldstein_score)})
     .attr('r', 5)
     .attr('fill', 'red')
-
-  advance();
-
 }
 
+GlobalGoldstein.prototype.update = function() {
+  var x = this.x;
+  var y = this.y;
 
-function advance() {
+  if (index > 75) {
+    this.svg.select('g')
+      .transition()
+      .delay(interval)
+      .duration(interval/2)
+      .attr('transform', 'translate('+x(index-75)*-1+',0)')
+
+    this.svg.select('path')
+      .datum(data.slice(index-75, index+25))
+      .attr('d', this.line)
+  }
+
+  this.svg.select('circle').data([data[index]])
+    .transition()
+    .duration(interval/2)
+    .attr('cx', function(d){ return x(+d.num)})
+    .attr('cy', function(d){ return y(+d.goldstein_score)})
+};
+
+//End GlobalGoldstein
+
+
+/*
+ * MainText()
+ * Main text visualizer
+ * Shows text for actors and events
+ *
+ */
+
+function MainText() {
+  //nothing to do here
+}
+
+MainText.prototype.update = function() {
   var main_text = d3.select('body').select('#main-text').text('');
 
   var actor1 = data[index].actor1,
@@ -68,33 +137,10 @@ function advance() {
   main_text.append('p').text('Victim: ' + actor2);
   main_text.append('p').text('Event: ' + data[index].event_description.replace(', not specified below', '').toUpperCase());
   main_text.append('p').text('Original: ' + data[index].orignal_text);
+};
 
-  //if (svg.select('circle').attr('cx') > width * .75) {
-  if (index > 75) {
-    svg.select('g')
-      .transition()
-      .delay(interval)
-      .duration(interval/2)
-      .attr('transform', 'translate('+x(index-75)*-1+',0)')
+// end main text visualizer
 
-    svg.select('path')
-      .datum(data.slice(index-75, index+25))
-      .attr('d', line)
-  }
-
-  svg.select('circle').data([data[index]])
-    .transition()
-    .duration(interval/2)
-    .attr('cx', function(d){ return x(+d.num)})
-    .attr('cy', function(d){ return y(+d.goldstein_score)})
-
-  index ++;
-  if (index >= data.length) index = 0;
-
-  setTimeout(advance, interval);
-}
-
-//var lineFunction = d3.svg.line().x(x).y(y);
 
 function filter(column, val) {
   return data.filter(function(item){
